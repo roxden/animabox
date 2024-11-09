7a0cd00b930a855c5ab7a387b166ad02d22db755
roxden <fachrum2898@yahoo.com>
2024-11-09 21:05:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script untuk menambahkan interaktivitas, misalnya handling klik atau efek lain.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website AnimeStream loaded!");
+});
